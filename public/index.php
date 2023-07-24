155dccfb64e00c337ead3502c3c1fad57252324a
NoahSavant <xayvier01@gmail.com>
2023-07-24 13:47:59 +0700

    create project

---
+++ public/index.php
@@ -0,0 +1,14 @@
+
+<?php
+
+require_once '../vendor/autoload.php';
+require_once  '../config/config.php' ;
+// Autoloader
+require_once '../vendor/autoload.php';
+
+// Load Config
+require_once '../config/config.php';
+
+// Routes
+require_once '../routes/web.php';
+require_once '../app/Router.php';
